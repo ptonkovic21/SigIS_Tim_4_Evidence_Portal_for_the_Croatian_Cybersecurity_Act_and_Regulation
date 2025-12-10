77aa9d2fd5d48bf16935d06a3760e8cf0f7650ba
Petar Tonković <ptonkovic21@student.foi.hr>
2025-12-10 13:21:10 +0100

    Added placeholder html and js generated with ai. Minor name changes

--- WebApp/server.js
+++ WebApp/server.js
@@ -8,10 +8,34 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = 3000;
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "")));
 
 app.get("/", (req, res) => {
-	res.send("./index.html");
+	res.redirect("/dashboard");
+});
+
+app.get("/index", (req, res) => {
+	res.redirect("/dashboard");
+});
+
+app.get("/index.html", (req, res) => {
+	res.redirect("/dashboard");
+});
+
+app.get("/dashboard", (req, res) => {
+	res.sendFile(path.join(__dirname, "dashboard.html"));
+});
+
+app.get("/catalog", (req, res) => {
+	res.sendFile(path.join(__dirname, "./html/catalog.html"));
+});
+
+app.get("/export", (req, res) => {
+	res.sendFile(path.join(__dirname, "./html/export.html"));
+});
+
+app.get("/obligation", (req, res) => {
+	res.sendFile(path.join(__dirname, "./html/obligation.html"));
 });
 
 app.listen(PORT, () => {
